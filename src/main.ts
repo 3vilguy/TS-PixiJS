e9f0c51ee766d4db783910adae59417cf72154ad
3vilguy <3vilguy@gmail.com>
2017-11-06 23:01:23 +0000

    Fix some styling, so we don't see scroll bar.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ const HEIGHT : number = window.innerHeight;
 const BG_COLOR = 0xc1c2c4;
 
 const renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT, { backgroundColor: BG_COLOR });
+renderer.view.style.display = "block";
 document.body.appendChild(renderer.view);
 
 const stage = new PIXI.Container();
