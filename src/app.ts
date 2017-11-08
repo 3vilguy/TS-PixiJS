ef00015e08743f2b2abd2ca468163d618b82978f
3vilguy <3vilguy@gmail.com>
2017-11-08 23:58:17 +0000

    Using PIXI.Application again.

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,18 @@
 import * as PIXI from 'pixi.js';
 
-var renderer = PIXI.autoDetectRenderer(800, 600, {backgroundColor: 0x1099bb});
-document.body.appendChild(renderer.view);
-
-var stage = new PIXI.Container();
+const app = new PIXI.Application(800, 600, {backgroundColor: 0x1099bb});
+document.body.appendChild(app.view);
 
 var graph = new PIXI.Graphics();
 graph.beginFill(0xFF0000);
 graph.drawRect(0, 0, 100, 100);
 graph.endFill();
-stage.addChild(graph);
+app.stage.addChild(graph);
 graph.x = graph.y = 100;
-animate();
 
-function animate() {
-    requestAnimationFrame(animate);
+app.ticker.add(update);
+
+function update() {
     graph.rotation += 0.05;
-    renderer.render(stage);
+    app.renderer.render(app.stage);
 }
