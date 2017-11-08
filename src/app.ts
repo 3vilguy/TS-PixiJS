81d5971a564c29db61d19f9bfe42878e68129a95
3vilguy <3vilguy@gmail.com>
2017-11-08 23:22:26 +0000

    Trying to get PIXI working with webpack.

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,20 @@
-document.body.innerHTML = `<p>Hi there. Current date is ${new Date()}.</p>`;
-console.log('hello console');
+import * as PIXI from 'pixi.js';
+
+var renderer = PIXI.autoDetectRenderer(800, 600, {backgroundColor: 0x1099bb});
+document.body.appendChild(renderer.view);
+
+var stage = new PIXI.Container();
+
+var graph = new PIXI.Graphics();
+graph.beginFill(0xFF0000);
+graph.drawRect(0, 0, 100, 100);
+graph.endFill();
+stage.addChild(graph);
+graph.x = graph.y = 100;
+animate();
+
+function animate() {
+    requestAnimationFrame(animate);
+    graph.rotation += 0.05;
+    renderer.render(stage);
+}
