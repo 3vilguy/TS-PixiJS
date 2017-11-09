1e49f620f6c655e1aea53283282e882b0ba9e987
3vilguy <3vilguy@gmail.com>
2017-11-09 00:20:04 +0000

    Add resizing stuff again.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,41 @@
 import * as PIXI from 'pixi.js';
 
-const app = new PIXI.Application(800, 600, {backgroundColor: 0x1099bb});
-document.body.appendChild(app.view);
+// GLOBALS
+const WIDTH : number = 800;
+const HEIGHT : number = 600;
+const BG_COLOR : number = 0x1099bb;
 
+// Init Pixi stuff
+const app = new PIXI.Application(WIDTH, HEIGHT, {backgroundColor: BG_COLOR});
+app.view.style.display = "block";
+app.renderer.autoResize = true;
+document.body.appendChild(app.view);
+onWindowResize();
+
+// Resizing
+if (app.renderer.autoResize) {
+    window.onresize = onWindowResize;
+}
+
+function onWindowResize() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    app.renderer.resize(width, height);
+    app.view.style.width = width + "px";
+    app.view.style.height = height + "px";
+
+    app.stage.x = (window.innerWidth - WIDTH) * 0.5;
+}
+
+
+// Other stuff
 var graph = new PIXI.Graphics();
 graph.beginFill(0xFF0000);
 graph.drawRect(0, 0, 100, 100);
 graph.endFill();
 app.stage.addChild(graph);
-graph.x = graph.y = 100;
+graph.x = WIDTH * 0.5;
+graph.y = HEIGHT * 0.5;
 
 app.ticker.add(update);
 
